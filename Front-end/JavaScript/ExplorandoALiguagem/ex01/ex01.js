7e3b85c24704768471f0cf4d6d7d199ec1ccbcf6
Nathandisk <103308730+Nathandiassoares@users.noreply.github.com>
2022-08-23 21:18:17 -0300

    Explorando a linguagem- Variaveis

---
+++ Front-end/JavaScript/ExplorandoALiguagem/ex01/ex01.js
@@ -0,0 +1,4 @@
+console.log("Olá Mundo")
+let idade = 10
+console.log(idade)
+console.log(idade+10)
---
+++ Front-end/JavaScript/ExplorandoALiguagem/ex02.js
@@ -0,0 +1,4 @@
+console.log("Trabalhando com variaveis")
+// JavaScrip é Case sensivtive
+const nome = 'Nathan'
+console.log(nome)
