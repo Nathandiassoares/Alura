29077cf31ef2862e6bb82442904baf8fe0a8c314
Nathandisk <103308730+Nathandiassoares@users.noreply.github.com>
2022-09-18 17:03:09 -0300

    Um pouco sobre metodos e mudanças em contato do site da barbearia alura

--- Front-end/JavaScript/JavaScript-basico/Orietaçao-A-Objetos/index.js
+++ Front-end/JavaScript/JavaScript-basico/Orietaçao-A-Objetos/index.js
@@ -1,15 +1,36 @@
 class Cliente {
     nome
     cpf
-    agencia
-    saldo
 }
 
-const cliente1 = new Cliente()
+class contaCorrente {
+    agencia
+    _saldo = 0 //usando "_" "anderlane"/ "#" considera privado assim não podendo ser alterado fora da class apenas pela função.
+    sacar(valor){ // função sacar para evitar saques acima do saldo na conta
+        if(this._saldo >= valor){
+         this._saldo -= valor   
+        }
+    }
+  
+    depositar(valor){ //função depositar,para evitar numeros negativos idesejados
+        if(valor > 0){
+            this._saldo += valor 
+        }
+    }
+}
 
+
+const cliente1 = new Cliente()
 cliente1.nome    = 'Nathan'
 cliente1.cpf     = 10000000001
-cliente1.agencia = 1001
-cliente1.saldo   = 0
 
-console.log(`ola ${cliente1.nome}`)
+const contaCorrenteNathan = new contaCorrente()
+contaCorrenteNathan.agencia = 1001
+
+contaCorrenteNathan.depositar(1000)
+console.log(`Saldo ${contaCorrenteNathan._saldo}`)
+
+contaCorrenteNathan.sacar(100)
+console.log(`Saldo ${contaCorrenteNathan._saldo}`)
+
+
