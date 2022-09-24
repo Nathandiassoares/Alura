0bd6e8ddfc065f2b9bb8b1634e763886f86b2d29
Nathandisk <103308730+Nathandiassoares@users.noreply.github.com>
2022-09-24 14:55:16 -0300

    Update index.js

--- Front-end/JavaScript/JavaScript-basico/Orietaçao-A-Objetos/index.js
+++ Front-end/JavaScript/JavaScript-basico/Orietaçao-A-Objetos/index.js
@@ -2,23 +2,23 @@ class Cliente {
     nome
     cpf
 }
-
 class contaCorrente {
     agencia
     _saldo = 0 //usando "_" "anderlane"/ "#" considera privado assim não podendo ser alterado fora da class apenas pela função.
     sacar(valor){ // função sacar para evitar saques acima do saldo na conta
         if(this._saldo >= valor){
          this._saldo -= valor   
+         return valor 
         }
     }
   
     depositar(valor){ //função depositar,para evitar numeros negativos idesejados
-        if(valor > 0){
-            this._saldo += valor 
+        if(valor <= 0){
+            return
         }
+        this._saldo += valor 
     }
 }
-
 
 const cliente1 = new Cliente()
 cliente1.nome    = 'Nathan'
@@ -30,7 +30,13 @@ contaCorrenteNathan.agencia = 1001
 contaCorrenteNathan.depositar(1000)
 console.log(`Saldo ${contaCorrenteNathan._saldo}`)
 
-contaCorrenteNathan.sacar(100)
+const valorSacado = contaCorrenteNathan.sacar(100)
 console.log(`Saldo ${contaCorrenteNathan._saldo}`)
+console.log(`Valor sacado ${valorSacado}`)
+
+
+
+
+
 
 
