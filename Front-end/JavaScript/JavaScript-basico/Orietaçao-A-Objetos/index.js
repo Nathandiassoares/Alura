7887c5c0be7e90349bb05195366c79c7f2522738
Nathandisk <103308730+Nathandiassoares@users.noreply.github.com>
2022-10-06 20:26:06 -0300

    New past

--- Front-end/JavaScript/JavaScript-basico/Orietaçao-A-Objetos/index.js
+++ Front-end/JavaScript/JavaScript-basico/Orietaçao-A-Objetos/index.js
@@ -1,57 +1,28 @@
-class Cliente {
-    nome
-    cpf
-}
-class contaCorrente {
-    agencia
-    _saldo = 0 //usando "_" "anderlane"/ "#" considera privado assim não podendo ser alterado fora da class apenas pela função.
-    sacar(valor){ // função sacar para evitar saques acima do saldo na conta
-        if(this._saldo >= valor){
-         this._saldo -= valor   
-         return valor 
-        }
-    }
-  
-    depositar(valor){ //função depositar,para evitar numeros negativos idesejados
-        if(valor <= 0){
-            return
-        }
-        this._saldo += valor 
-    }
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
-    transferir (valor, conta) {  //valor e conta são demostrativo não são variaveis
-        const valorSacado = this.sacar(valor) // valor e demotrativo
-        conta.depositar(valorSacado) // conta e demotrativo
-    }//adcionamos o que queremos usar logo abaixo como sacando criando uma variavel para retirar o valor da conta que ira tranferir e logo abaixo depositar na conta que recebera o valor
-    
-}
+const cliente1 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
 
-const cliente01 = new Cliente()  //Adcionando novo cliente
-cliente01.nome = 'Nathan'
-cliente01.cpf = 333111
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
 
 
-const cliente02 = new Cliente() //Adcionando novo cliente
-cliente02.nome = 'Aline'
-cliente02.cpf = 333222
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
 
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente1;
+conta2.agencia = 102;
 
+let valor = 200;
+contaCorrenteRicardo.tranferir(valor, conta2);
 
-const contaCorrenteAline = new contaCorrente()  //Nova conta corrente
-contaCorrenteAline.cliente = cliente01
-contaCorrenteAline.agencia = 101 
-
-const contaCorrenteNathan = new contaCorrente() //Nova conta corrente
-contaCorrenteNathan.cliente = cliente02
-contaCorrenteNathan.agencia = 102
-
-
-
-contaCorrenteAline.depositar(300)
-
-console.log(contaCorrenteAline)
-
-contaCorrenteAline.transferir(300,contaCorrenteNathan)
-
+conta2.saldo = 30000;
+console.log(conta2.saldo);
 
 
