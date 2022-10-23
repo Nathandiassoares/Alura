021d5bbf1db37ef50adaeb05c7c83ebc2b6a1711
Nathandisk <103308730+Nathandiassoares@users.noreply.github.com>
2022-10-23 08:58:30 -0300

    Projeto local storege finalizado

--- Front-end/JavaScript/JavaScript-basico/mochila-de-viagem-main/js/main.js
+++ Front-end/JavaScript/JavaScript-basico/mochila-de-viagem-main/js/main.js
@@ -1,58 +1,57 @@
 const form = document.getElementById("novoItem")
-const lista = document.getElementById("lista") // busacamos a tag pelo ID e adcionamos o elemento dentro com appChild
-const itens = JSON.parse(localStorage.getItem("itens")) || [] // verificamos se a itens antes de adcionar algo dentro do array, e com "JSON parse" tranformamos os dados em codigo novamente
+const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []
 
-itens.forEach((element) => {
-        criaElemento(element)
+itens.forEach((elemento) => {
+    criaElemento(elemento)
 })
 
 
-
-form.addEventListener("submit",(evento) => { //addevent cria um evento corespondente ao valor da teg subimit, o evento se refere para o que esta detron
-    evento.preventDefault()//usando o prevdefault conseguimos manter do mesmo jeito que ele e(nao envia)
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
 
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
-    
-    const existe = itens.find(elemento => elemento.value === nome.value)
+
+    const existe = itens.find( elemento => elemento.nome === nome.value )
+
     const itemAtual = {
-            "nome": nome.value,
-            "quantidade": quantidade.value
-        }
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
 
-        if(existe){
-            itemAtual.id = existe.id
+    if (existe) {
+        itemAtual.id = existe.id
+        
+        atualizaElemento(itemAtual)
 
-            atualizaElemento(itemAtual)
+        itens[existe.id] = itemAtual
+    } else {
+        itemAtual.id = itens.length
 
-            itens[existe.id] = itemAtual
-        }else {
-            itemAtual.id = itens.length
+        criaElemento(itemAtual)
 
-            criaElemento(itemAtual)
-            itens.push(itemAtual)
-        }
-       
-    localStorage.setItem("item",JSON.stringify(itens))//JSON.stringfy e usado para tranforma nosso codigo/dados em string
+        itens.push(itemAtual)
+    }
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 
     nome.value = ""
     quantidade.value = ""
 })
 
 function criaElemento(item) {
-    const novoItem = document.createElement("li")//Cria um elemento/tag no HTML
-    novoItem.classList.add("item") // Cria uma class com nome item no HTML
+    const novoItem = document.createElement("li")
+    novoItem.classList.add("item")
 
-    const numeroItem = document.createElement('strong') //criamos uma variavel para conter um novo elemento
-    
-    numeroItem.innerHTML = item.quantidade //colocamos dentro do novo elemento a quantidade que esta sendo adquirida
-
+    const numeroItem = document.createElement("strong")
+    numeroItem.innerHTML = item.quantidade
     numeroItem.dataset.id = item.id
+    novoItem.appendChild(numeroItem)
     
-    novoItem.appendChild(numeroItem) // como o javascript cria um objeto precisamos tranforma ele em um elemento para usamos por isso o appdChid..
-    novoItem.innerHTML  += item.nome
+    novoItem.innerHTML += item.nome
 
-    lista.appendChild(novoItem) 
+    lista.appendChild(novoItem)
 }
 
 function atualizaElemento(item) {
